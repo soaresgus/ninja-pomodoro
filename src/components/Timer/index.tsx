15afb353a13272f6ecc6e0672f888529915dda50
soaresgus <gustavosoaresbt@hotmail.com>
2022-07-17 01:27:24 -0300

    Corrections on rest timer; Added prettier;

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,6 +1,13 @@
-import { ConfigurationsContainer, Container, ControlersContainer, PlayPauseButton, ResetButton, TimeText } from "./styles";
+import {
+  ConfigurationsContainer,
+  Container,
+  ControlersContainer,
+  PlayPauseButton,
+  ResetButton,
+  TimeText,
+} from "./styles";
 
-import { MdTune, MdPlayArrow, MdPause, MdRefresh } from 'react-icons/md'
+import { MdTune, MdPlayArrow, MdPause, MdRefresh } from "react-icons/md";
 import { IconContext } from "react-icons";
 
 import { PopoverButton } from "../PopoverButton";
@@ -11,53 +18,59 @@ import usePersistedState from "../../utils/usePersistedState";
 import { useTimer } from "../../utils/useTimer";
 
 export function Timer() {
-    const timer = useTimer();
-    const [blockRest, setBlockRest] = usePersistedState('blockRest', true)
+  const timer = useTimer();
+  const [blockRest, setBlockRest] = usePersistedState("blockRest", true);
 
-    return (
-        <IconContext.Provider value={{ size: '28' }}>
-            <Container>
-                <TimerModeTabs
-                    content={[
-                        (
-                            <TimeText>{timer.formatSeconds(timer.actualJobTime, true, true)}</TimeText>
-                        ),
-                        (
-                            <TimeText>{timer.formatSeconds(timer.actualRestTime, true, true)}</TimeText>
-                        )
-                    ]}
-                    disabled={[false, (timer.actualJobTime > 0 && blockRest)]}
-                    onClickActions={[() => timer.resetTimer(timer.persistedJobTime), () => timer.resetTimer(timer.persistedRestTime)]}
-                />
-                <ControlersContainer>
-                    <PlayPauseButton
-                        onClick={() => {
-                            timer.setPlaying(!timer.playing);
-                        }}
-                    >
-                        {
-                            timer.playing ? (<MdPause />) : (<MdPlayArrow />)
-                        }
-                    </PlayPauseButton>
+  return (
+    <IconContext.Provider value={{ size: "28" }}>
+      <Container>
+        <TimerModeTabs
+          content={[
+            <TimeText>
+              {timer.formatSeconds(timer.actualJobTime, true, true)}
+            </TimeText>,
+            <TimeText>
+              {timer.formatSeconds(timer.actualRestTime, true, true)}
+            </TimeText>,
+          ]}
+          disabled={[false, timer.actualJobTime > 0 && blockRest]}
+          onClickActions={[
+            () => {
+              timer.resetTimer(timer.persistedJobTime);
+              timer.setRestMode(false);
+            },
+            () => {
+              timer.resetTimer(timer.persistedRestTime);
+              timer.setRestMode(true);
+            },
+          ]}
+        />
+        <ControlersContainer>
+          <PlayPauseButton
+            onClick={() => {
+              timer.setPlaying(!timer.playing);
+            }}
+          >
+            {timer.playing ? <MdPause /> : <MdPlayArrow />}
+          </PlayPauseButton>
 
-                    <ResetButton
-                        onClick={() => {
-                            timer.restMode ? timer.resetTimer(timer.persistedRestTime) : timer.resetTimer(timer.persistedJobTime)
-                        }}
-                    >
-                        <MdRefresh />
-                    </ResetButton>
-                </ControlersContainer>
+          <ResetButton
+            onClick={() => {
+              timer.restMode
+                ? timer.resetTimer(timer.persistedRestTime)
+                : timer.resetTimer(timer.persistedJobTime);
+            }}
+          >
+            <MdRefresh />
+          </ResetButton>
+        </ControlersContainer>
 
-                <ConfigurationsContainer>
-                    <PopoverButton
-                        icon={<MdTune />}
-                        title='Configurações'
-                    >
-                        <ConfigurationsForm />
-                    </PopoverButton>
-                </ConfigurationsContainer>
-            </Container>
-        </IconContext.Provider >
-    )
+        <ConfigurationsContainer>
+          <PopoverButton icon={<MdTune />} title="Configurações">
+            <ConfigurationsForm />
+          </PopoverButton>
+        </ConfigurationsContainer>
+      </Container>
+    </IconContext.Provider>
+  );
 }
