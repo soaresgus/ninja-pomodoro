a8305af53786cb7bbdb2d6fcde7d729f11b4062a
soaresgus <gustavosoaresbt@hotmail.com>
2022-07-30 21:35:28 -0300

    Corrections on identation

--- src/components/PopoverButton/index.tsx
+++ src/components/PopoverButton/index.tsx
@@ -1,48 +1,63 @@
-import { HoverButtonArrow, HoverButtonContent, HoverButtonTrigger, PopoverArrow, PopoverClose, PopoverContent, PopoverTrigger } from './styles'
+import {
+  HoverButtonArrow,
+  HoverButtonContent,
+  HoverButtonTrigger,
+  PopoverArrow,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from './styles';
 
 import * as HoverCard from '@radix-ui/react-hover-card';
 import * as Popover from '@radix-ui/react-popover';
 
-import { MdClose } from 'react-icons/md'
+import { MdClose } from 'react-icons/md';
 
 interface PopoverProps extends React.ComponentProps<typeof Popover.Root> {
-    title?: string,
-    icon: any,
-    children?: React.ReactNode,
-    paleteColor?: 'primary' | 'secondary' | 'hover' | 'light' | 'text',
-    avoidCollisions?: boolean
+  title?: string;
+  icon: React.ReactNode;
+  children?: React.ReactNode;
+  paleteColor?: 'primary' | 'secondary' | 'hover' | 'light' | 'text';
+  avoidCollisions?: boolean;
 }
 
-export function PopoverButton({ title, icon, children, paleteColor, avoidCollisions = true, ...props }: PopoverProps) {
-    const HoverButton = HoverCard.Root;
+export function PopoverButton({
+  title,
+  icon,
+  children,
+  paleteColor,
+  avoidCollisions = true,
+  ...props
+}: PopoverProps) {
+  const HoverButton = HoverCard.Root;
 
-    const PopoverRoot = Popover.Root;
-    return (
-        <PopoverRoot modal {...props}>
-            <PopoverTrigger aria-label={title}>
-                <HoverButton openDelay={0}>
-                    <HoverButtonTrigger>
-                        {icon}
-                    </HoverButtonTrigger>
+  const PopoverRoot = Popover.Root;
+  return (
+    <PopoverRoot modal {...props}>
+      <PopoverTrigger aria-label={title}>
+        <HoverButton openDelay={0}>
+          <HoverButtonTrigger>{icon}</HoverButtonTrigger>
 
-                    {
-                        title && (
-                            <HoverButtonContent color={paleteColor}>
-                                <span>{title}</span>
-                                <HoverButtonArrow />
-                            </HoverButtonContent>
-                        )
-                    }
-                </HoverButton>
-            </PopoverTrigger>
+          {title && (
+            <HoverButtonContent color={paleteColor}>
+              <span>{title}</span>
+              <HoverButtonArrow />
+            </HoverButtonContent>
+          )}
+        </HoverButton>
+      </PopoverTrigger>
 
-            <PopoverContent avoidCollisions={avoidCollisions} color={paleteColor} side='top'>
-                <PopoverClose>
-                    <MdClose size={15} />
-                </PopoverClose>
-                {children}
-                <PopoverArrow color={paleteColor} />
-            </PopoverContent>
-        </PopoverRoot>
-    )
+      <PopoverContent
+        avoidCollisions={avoidCollisions}
+        color={paleteColor}
+        side="top"
+      >
+        <PopoverClose>
+          <MdClose size={15} />
+        </PopoverClose>
+        {children}
+        <PopoverArrow color={paleteColor} />
+      </PopoverContent>
+    </PopoverRoot>
+  );
 }
